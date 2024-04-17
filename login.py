b56478848149ea59a62abae7623649f77e4df13a
Meirrs <147962255+Meirrs@users.noreply.github.com>
2024-04-17 20:20:23 +0800

    Update login.py

--- login.py
+++ login.py
@@ -5,46 +5,41 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
-
-acccounts = int(len(sys.argv[1:])/2)
+# 计算账户数量
+acccounts = int(len(sys.argv[1:]) / 2)
 print(f'Config {acccounts} accounts')
+
 for i in range(acccounts):
-    email = sys.argv[1+i]
-    passwd = sys.argv[1+i+acccounts]
+    email = sys.argv[1 + i]
+    passwd = sys.argv[1 + i + acccounts]
     print('----------------------------')
 
-    #1.open browser
+    # 1. 打开浏览器
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     driver.set_window_size(1000, 720)
     driver.get("https://game.maj-soul.net/1/")
-    print(f'Account {i+1} loading game...')
+    print(f'Account {i + 1} loading game...')
     sleep(10)
 
-    #2.input email
+    # 2. 输入邮箱
     screen = driver.find_element(By.ID, 'layaCanvas')
-    ActionChains(driver)\
-        .move_to_element_with_offset(screen, 250, -100)\
-        .click()\
-        .perform()
+    ActionChains(driver).move_to_element_with_offset(screen, 250, -100).click().perform()
     driver.find_element(By.NAME, 'input').send_keys(email)
     print('Input email successfully')
 
-    #3.input password
-    ActionChains(driver)\
-        .move_to_element_with_offset(screen, 250, -50)\
-        .click()\
-        .perform()
+    # 3. 输入密码
+    ActionChains(driver).move_to_element_with_offset(screen, 250, -50).click().perform()
     driver.find_element(By.NAME, 'input_password').send_keys(passwd)
     print('Input password successfully')
 
-    #4.login
-    ActionChains(driver)\
-        .move_to_element_with_offset(screen, 250, 50)\
-        .click()\
-        .perform()
+    # 4. 点击登录
+    ActionChains(driver).move_to_element_with_offset(screen, 250, 50).click().perform()
     print('Entering game...')
-    sleep(20) #loading...
+    sleep(20)  # 等待登录...
+
+    # 模拟点击后再等待20秒
+    sleep(20)
     print('Login success')
     driver.quit()
